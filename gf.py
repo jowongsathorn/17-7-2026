c06b066801841ffd49e1aa4e1ac02ae77492384e
Student <>
2026-07-17 14:00:03 +0700

    new

--- gf.py
+++ gf.py
@@ -3,3 +3,23 @@ joe = {"first name":"joe"
        "student_id":"69130500049",
        }
 print(joe)
+
+
+sicario_list = []
+member1 = {
+    "name":"Leaf",
+    "age": 19,
+    "faculty": "IT"
+}
+member2 = {
+    "name":"Mean",
+    "age": 20,
+    "faculty": "IT"
+}
+member3 = {
+    "name":"Somchi",
+    "age": 18,
+    "faculty": "DSI"
+}
+sicario_list.append(member3)
+sicario_list.append(member2)
